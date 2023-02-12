aa66960a43eb76eea5843318e55fa90332621236
Arondeke <khmelnitskiy.aleksey@gmail.com>
2023-02-12 11:11:10 +0300

    init

--- src/HW_objectsAndClasses/Author.java
+++ src/HW_objectsAndClasses/Author.java
@@ -1,4 +1,20 @@
 package HW_objectsAndClasses;
 
 public class Author {
+
+    private String firstName;
+    private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLasttName() {
+        return this.lastName;
+    }
 }
